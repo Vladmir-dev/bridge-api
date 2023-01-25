946fed860aab422194d3bcd8a4a7a3918c90c7ec
Vladmir-dev <tempestpius70@gmail.com>
2023-01-25 13:42:48 +0300

    added Wallet, Comment, Post, Likes & Chat models

--- bridge/admin.py
+++ bridge/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import User
 
+# class ProfileInline(admin.StackedInline):
+#     model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ['username', 'first_name', 'last_name', 'phone_number', 'email', 'sex', 'accepted_terms', 'date_of_birth', 'country', 'nationality', 'city', 'password']
+    # inlines = [ProfileInline]
+    
 # Register your models here.
-admin.site.register(User)
+# admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
+admin.site.unregister(Group)
--- bridge/models.py
+++ bridge/models.py
@@ -6,6 +6,7 @@ from django_countries.fields import CountryField
 from django.utils import timezone
 import re
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
 # Create your models here.
 
 class User(AbstractBaseUser):
@@ -23,10 +24,88 @@ class User(AbstractBaseUser):
     date_of_birth = models.DateField(blank=True, null=True)
     country = CountryField(blank=True)
     nationality = CountryField(blank=True)
+    is_active = models.BooleanField(default=False)
     city = models.CharField(max_length=200, blank=True)
     token = models.CharField(max_length=1000, null=True)
+    photo = models.ImageField(upload_to="media", null=True, blank=True)
+    background_photo = models.ImageField(upload_to="media", null=True, blank=True)
+    anonymous = models.BooleanField(default=False)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+class ChatMessage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipient")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{0} : {1}".format(self.user, self.message)
+
+
+class RelationShip(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed")
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField(default=0)
+    total_sent = models.FloatField(default=0)
+    total_received = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return "{o} wallet".format(self.user)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.comment
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post
+
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bridges = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+#     photo = models.ImageField(upload_to="media",null=True, blank=True,)
+#     background_photo = models.ImageField(upload_to="media",null=True, blank=True,)
+
+#     def __str__(self):
+#         return self.user.username
+
+
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         user_profile = Profile(user=instance)
+#         user_profile.save()
+#         user_profile.bridges.set([instance.profile.id])
+#         user_profile.save()
+
+# post_save.connect(create_profile, sender=User)
